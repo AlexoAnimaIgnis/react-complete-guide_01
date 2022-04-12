60eafd9e3d72efbc0a64b15c153e46d66018ff4e
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-04-12 15:37:33 +0800

    Adding styles to a custom component

--- src/components/ExpsenseItem.js
+++ src/components/ExpsenseItem.js
@@ -1,9 +1,20 @@
+import './ExpenseItem.css'; // tells built process that css file should be considered
+
 /**
  * A component in react is just a javascript function which return an HTML code (JSx)
  * @returns 
  */
 function ExpenseItem() {
-    return <h2>Expense Item</h2>
+    // react rule: you must only have one root element per JSX code snippet
+    return (
+        <div className='expense-item'>
+            <div>April 11, 2022</div>
+            <div className='expense-item__description'>
+                <h2>Car Insurance</h2>
+                <div className='expense-item__price'>$2900.00</div>
+            </div>
+        </div>
+    );
 }
 
 /**
