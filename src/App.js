49c5f94e045c9b869f20b11e1b03299324071c66
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-04-12 15:25:16 +0800

    Building First Custom component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+
+// Add the following line in order to use the custom component like a regular component
+import ExpenseItem from "./components/ExpsenseItem"; // lowercase elements are considered built-in HTML elements. use uppercase
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also available!</p>
+      <ExpenseItem />
     </div>
   );
 }
---
+++ src/components/ExpsenseItem.js
@@ -0,0 +1,13 @@
+/**
+ * A component in react is just a javascript function which return an HTML code (JSx)
+ * @returns 
+ */
+function ExpenseItem() {
+    return <h2>Expense Item</h2>
+}
+
+/**
+ * add the following line, to be able to use the component. otherwise, this will
+ * only be usable in this file.
+ */
+export default ExpenseItem;
